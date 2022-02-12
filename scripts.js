f3941cb764c6ac45f22d5d07e80d76ccb14cb4e6
unknown <govind.bisen@gmail.com>
2022-02-12 22:32:01 +0530

    20 button 100 record 5 people per button in console

--- scripts.js
+++ scripts.js
@@ -13,13 +13,26 @@ async function pagination() {
   const noOfPeoplePerPage = 5;
   const currentPage = 1;
 
+  function displayPeople(i) {
+    console.log(i);
+    for (
+      let index = i * noOfPeoplePerPage - 5;
+      index <= i * noOfPeoplePerPage - 1;
+      index++
+    ) {
+      const element = data[index];
+      console.log(element);
+    }
+  }
+
   for (
     let i = currentPage;
-    i <= data.length / noOfPeoplePerPage + currentPage;
+    i <= data.length / noOfPeoplePerPage + currentPage - 1;
     i++
   ) {
     const button = document.createElement("BUTTON");
     button.innerText = `${i}`;
+    button.addEventListener("click", () => displayPeople(i));
     container.appendChild(button);
   }
 }
@@ -27,6 +40,12 @@ pagination();
 
 container.appendChild(p);
 
-// i=1 page -> data 0-4 => loop => (i*noOfPeoplePerPage - 1) to (i*noOfPeoplePerPage - 1)+5
+// i=1 page -> data 0-4 => loop => (i*noOfPeoplePerPage) to (i*noOfPeoplePerPage - 1)+5
 // i=2 page -> data 5-9
 // i=3 page -> data 10-14
+
+// i = 1 ; 1,2,3,4,5
+// i = 2 ; 6,7,8,9,10
+// i = 3 ; 11.12.13.14.15
+// i = 4 ; 16,17,18,19,20
+// i = 5 ; 21,22,23,24,25
